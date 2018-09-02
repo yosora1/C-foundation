f4aa2c2752c22cdca3663e472c7bcd5747086ffc
钟凯贺 <41095544+yosola1@users.noreply.github.com>
2018-09-02 16:03:25 +0800

    DataAdapter

---
+++ DataAdapterUse/DataAdapter/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DataAdapter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String constr = "Data Source=DESKTOP-40POHN9;Initial Catalog=Test;Integrated Security=True";
+            String sql = "select * from UserTable";
+            DataTable dt = new DataTable();
+            using (SqlDataAdapter adapter=new SqlDataAdapter(sql,constr))
+            {
+                adapter.Fill(dt);
+            }
+            this.dataGridView1.DataSource = dt;
+        }
+    }
+}
---
+++ DataAdapterUse/DataSetAndDataTable/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSetAndDataTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建一个Dataset,DataSet就是一个临时数据库
+            DataSet ds = new DataSet("School");
+            //创建一张表
+            DataTable dt = new DataTable("Student");
+            //向表中创建列
+            DataColumn dcAutoId = new DataColumn("AutoId", typeof(int));
+            dcAutoId.AutoIncrement = true;
+            dcAutoId.AutoIncrementSeed = 1;
+            dcAutoId.AutoIncrementStep = 1;
+            dt.Columns.Add(dcAutoId);
+            DataColumn dcUserName=dt.Columns.Add("Name",typeof(String));
+            dcUserName.AllowDBNull = false;
+
+            //向表中增加行
+            DataRow one=dt.NewRow();
+            one["Name"]="ABC";
+            dt.Rows.Add(one);
+            DataRow tow = dt.NewRow();
+            tow["Name"] = "QWE";
+            dt.Rows.Add(tow);
+
+            //将表加入数据库
+            ds.Tables.Add(dt);
+
+            for (int i = 0; i < ds.Tables.Count; i++)
+            {
+                Console.WriteLine("TableName:",ds.Tables[i].TableName);
+                for (int j = 0; j < ds.Tables[i].Rows.Count; j++)
+                {
+                    DataRow row = ds.Tables[i].Rows[j];
+                    for (int c = 0; c < dt.Columns.Count; c++)
+                    {
+                        Console.Write(row[c]+"     || ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
